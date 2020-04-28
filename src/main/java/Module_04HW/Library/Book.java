14e05421e017efa7da5852b678bdfaa447501cf0
Tasha-Sh <46695598+Tasha-Sh@users.noreply.github.com>
2020-04-28 23:44:51 +0300

    +

--- src/main/java/Module_04HW/Library/Book.java
+++ src/main/java/Module_04HW/Library/Book.java
@@ -1,5 +1,10 @@
 package Module_04HW.Library;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Book {
     private String name;
     private String author;
@@ -15,5 +20,41 @@ public class Book {
         this.yearOfPublish = yearOfPublish;
         this.pageNumber = pageNumber;
         this.price = price;
+    }
+
+    List<Book> book = Arrays.asList(
+            new Book("Супер книга1", "Пупкин", "Кукуха", 1900, 200, 100),
+            new Book("Супер-пупер книга1", "Вася", "Некукуха", 900, 1000, 50),
+            new Book("Просто книга1", "Егорка", "Какое-то", 2020, 10, 20),
+            new Book("Супер книга2", "Пупкин", "Кукуха", 1950, 220, 120),
+            new Book("Супер-пупер книга2", "Вася", "Некукуха", 900, 1000, 30),
+            new Book("Просто книга2", "Егорка", "Какое-то", 2020, 10, 200),
+            new Book("Супер книга3", "Пупкин", "Кукуха", 1900, 250, 100),
+            new Book("Супер-пупер книга3", "Вася", "Некукуха", 900, 1000, 50),
+            new Book("Просто книга3", "Егорка", "Какое-то", 2020, 10, 80),
+            new Book("Супер книга4", "Пупкин", "Кукуха", 1900, 200, 100),
+            new Book("Супер-пупер книга4", "Вася", "Некукуха", 900, 1000, 500),
+            new Book("Просто книга4", "Егорка", "Какое-то", 2020, 10, 50)
+    );
+
+    public String searchOfBookName(String name, List<Book> books) {
+        books = new ArrayList<>();
+        for (String name : books) {
+            if (name.toLowerCase().contains(name.toLowerCase())) {
+                System.out.println(books);
+            }else {
+                System.out.println("Такой книги у нас нет");
+            }
+        }return book;
+    }
+    public void searchOfBookAuthor(String author, List<Book> books) {
+        books = new ArrayList<>();
+        for (String author : books) {
+            if (name.toLowerCase().contains(name.toLowerCase())) {
+                System.out.println(books);
+            }else {
+                System.out.println("Такой книги у нас нет");
+            }
+        }
     }
 }
--- src/main/java/Module_04HW/Library/Library.java
+++ src/main/java/Module_04HW/Library/Library.java
@@ -1,12 +1,19 @@
 package Module_04HW.Library;
 
-public class Library {
-    Book book1 = new Book("name1", "author1", "publishingHouse1", 2001, 101, 110);
-    Book book2 = new Book("name2", "author2", "publishingHouse2", 2002, 102, 120);
-    Book book3 = new Book("name3", "author3", "publishingHouse3", 2003, 103, 130);
-    Book book4 = new Book("name4", "author4", "publishingHouse4", 2004, 104, 140);
-    Book book5 = new Book("name5", "author5", "publishingHouse5", 2005, 105, 150);
-    Book book6 = new Book("name6", "author6", "publishingHouse6", 2006, 106, 160);
-    Book book7 = new Book("name7", "author7", "publishingHouse7", 2007, 107, 170);
+public static void main(String[]args){
+      String books;
+      book.add(new book("Книга","Кто-то","Неизвестное",1988,150,100));
 
-}
+//public class Library {
+//    Book book1 = new Book("name1", "author1", "publishingHouse1", 2001, 101, 110);
+//    Book book2 = new Book("name2", "author2", "publishingHouse2", 2002, 102, 120);
+//    Book book3 = new Book("name3", "author3", "publishingHouse3", 2003, 103, 130);
+//    Book book4 = new Book("name4", "author4", "publishingHouse4", 2004, 104, 140);
+//    Book book5 = new Book("name5", "author5", "publishingHouse5", 2005, 105, 150);
+//    Book book6 = new Book("name6", "author6", "publishingHouse6", 2006, 106, 160);
+//    Book book7 = new Book("name7", "author7", "publishingHouse7", 2007, 107, 170);
+//    String[] book = new String;
+
+
+        }
+        }
